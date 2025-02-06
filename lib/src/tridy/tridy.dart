efdf0b21d30f41a01bee6cb742d27c23bd802ba7
Matěj Verhaegen <matej@appelevate.cz>
2025-02-06 21:38:09 +0100

    freeze Jidelnicek()

--- lib/src/tridy/tridy.dart
+++ lib/src/tridy/tridy.dart
@@ -3,3 +3,4 @@ export 'alergen/alergen.dart';
 export 'jidlo_kategorizovano/jidlo_kategorizovano.dart';
 export 'login_data/login_data.dart';
 export 'burza/burza.dart';
+export 'jidelnicek/jidelnicek.dart';
