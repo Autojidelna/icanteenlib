6c96c1a6ffa79f43f978a6d1e6def81ac9326df5
mattheroit <mattheroit@pm.me>
2025-10-31 20:39:53 +0100

    remove support for v2.10.27

--- lib/src/canteen_versions.dart
+++ lib/src/canteen_versions.dart
@@ -1,7 +1,6 @@
 import 'package:icanteenlib/src/base_canteen.dart';
 
 final Map<String, Function(String, String)> canteenVersions = {
-  //'2.10.27': (url, webWerze) => Canteen2v10v27(url, webWerze),
   '2.16.15': (url, webWerze) => Canteen2v16v15(url, webWerze),
   '2.18.03': (url, webWerze) => Canteen2v18v03(url, webWerze),
   '2.18.19': (url, webWerze) => Canteen2v18v19(url, webWerze),
