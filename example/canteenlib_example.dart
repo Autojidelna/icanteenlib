36248ec76d31410988d25240a731bef884a64335
mattheroit <mattheroit@pm.me>
2025-10-31 03:00:07 +0100

    update example

--- example/canteenlib_example.dart
+++ example/canteenlib_example.dart
@@ -1,27 +1,82 @@
 import 'package:icanteenlib/canteenlib.dart';
 
-const String url = "kantyna.neco.cz";
+const String url = "jidelna.priklad.cz";
 const String username = "uzivatel";
-const String heslo = "heslo123";
+const String password = "heslo123";
 
 void main(List<String> args) async {
-  /// Vytvoření instance kantýny. Všechna komunikace probíhá skrz ni.
-  Canteen canteenInstance = Canteen(url);
+  /// Vytvoření instance kantýny
+  Canteen canteenInstance = await Canteen.create(url);
 
+  /// Přihlášení (je nutné pro fungování všech funkcí krom tedy zíkání veřejného jídelníčku)
   try {
-    // příhlášení
-    await canteenInstance.login(username, heslo); // přihlásit se
-
-    // získání jídelníčku pro daný den i s cenami
-    var jidelnicek = await canteenInstance.jidelnicekDen(den: DateTime.parse("2022-04-04"));
-
-    // získání informací o uživateli, jako je například kredit
-    print((await canteenInstance.ziskejUzivatele()).kredit);
-
-    // objednání jídla
-    var objednano = await canteenInstance.objednat(jidelnicek.jidla[0]);
-    print(objednano.jidla[0].objednano);
+    print(await canteenInstance.login(username, password) ? "login succesful" : "login failed"); // přihlásit se
   } catch (e) {
-    print("Při získávání informací nastala chyba: $e");
+    print('Login Failed - either bad url or no internet');
+    return;
   }
+  UzivatelskeUdaje? uzivatel;
+  try {
+    /// Získání informací o uživateli
+    uzivatel = await canteenInstance.ziskejUzivatelskeUdaje();
+    vytisknoutInfoOUzivateli(uzivatel);
+  } catch (e) {
+    print(e);
+  }
+
+  Jidelnicek? jidelnicek;
+  try {
+    /// Získání jídelníčku pro daný den
+    jidelnicek = await canteenInstance.specifickyJidelnicek(DateTime.now());
+    vytisknoutInfoOJidelnicku(jidelnicek);
+  } catch (e) {
+    print(e);
+  }
+  /*
+  |------------------------------------------------------------------------------------------------------------------|
+  |  Následující funkce objednávají obědy nebo dávají/odebírají jídla z burzy.                                       |
+  |  Buďte si proto jistí, že je chcete spustit, ať nepřijdete o oběd.                                               |
+  |                                                                                                                  |
+  |------------------------------------------------------------------------------------------------------------------|
+  */
+  try {
+    jidelnicek = await canteenInstance.provedObjednavku(jidelnicek!.nabidka[0]);
+    vytisknoutInfoOJidelnicku(jidelnicek);
+  } catch (e) {
+    print(e);
+  }
+}
+
+void vytisknoutInfoOJidelnicku(Jidelnicek jidelnicek) {
+  print('--------------jídelníček pro den ${jidelnicek.datum}--------------');
+  print('počet jídel: ${jidelnicek.nabidka.length}');
+  for (int i = 0; i < jidelnicek.nabidka.length; i++) {
+    print('------------Jídlo číslo $i------------');
+    print('název: ${jidelnicek.nabidka[i].nazev}');
+    print('cena: ${jidelnicek.nabidka[i].cena}');
+    print('varianta: ${jidelnicek.nabidka[i].varianta}');
+    print('datum: ${jidelnicek.nabidka[i].datum}');
+    for (int k = 0; k < jidelnicek.nabidka[i].alergeny.length; k++) {
+      print('alergen: ${jidelnicek.nabidka[i].alergeny[k].nazev} - ${jidelnicek.nabidka[i].alergeny[k].popis}');
+    }
+    print('url: ${jidelnicek.nabidka[i].url}');
+    if (jidelnicek.nabidka[i].slozeniJidla != null) {
+      print('Hlavní chod: ${jidelnicek.nabidka[i].slozeniJidla!.hlavniChod}');
+      print('pití: ${jidelnicek.nabidka[i].slozeniJidla!.piti}');
+      print('polévka: ${jidelnicek.nabidka[i].slozeniJidla!.polevka}');
+      print('Salátový bar: ${jidelnicek.nabidka[i].slozeniJidla!.salatovyBar}');
+      print('ostatní: ${jidelnicek.nabidka[i].slozeniJidla!.ostatni}');
+    }
+    print('--------------------------------------\n\n');
+  }
+  print('--------------konec jídelníčku--------------');
+}
+
+void vytisknoutInfoOUzivateli(UzivatelskeUdaje uzivatel) {
+  print('Jméno: ${uzivatel.jmeno}');
+  print('Příjmení: ${uzivatel.prijmeni}');
+  print('Kategorie: ${uzivatel.kategorie}');
+  print('Účet pro platby: ${uzivatel.ucetProPlatbyDoJidelny}');
+  print('Variabilní symbol: ${uzivatel.variabilniSymbol}');
+  print('Specifický symbol: ${uzivatel.specifickySymbol}');
 }
